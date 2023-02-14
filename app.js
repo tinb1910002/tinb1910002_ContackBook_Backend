54699bb30cd8474c3b00be12738530c0fa7cf781
Mai Trung Tin <tinb1910002@gmail.com>
2023-02-15 00:34:50 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors= require ("cors");
 
 const contactsRouter = require ("./app/routes/contact.route");
+const ApiError =require("./app/api-error");
 const app = express();
 
 app.use(cors());
@@ -10,5 +11,22 @@ app.get("/",(req,res) => {
     res.json({message:"Wellcome to contact book application."});
 });
 app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+    //code o day se chay khi khong co route duoc dinh nghia nao
+    // khop voi yeu cau. Goi next() de chuyem sang middleware xu ly loi
+    return next (new ApiError(404, "Resource not found"));
+});
+
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    //middleware xu ly loi tap trung.
+    //trong cac doan code xu ly o cac route, goi next(error)
+    //se chuyen ve middleware xu ly loi nay
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports=app;
